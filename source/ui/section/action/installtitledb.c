722eda2781c4c86a0dc229035067c3671843e96e
Vincysuper07 <vincysuper07@gmail.com>
2020-08-25 13:20:49 +0200

    cambiati i link... di nuovo...

--- source/ui/section/action/installtitledb.c
+++ source/ui/section/action/installtitledb.c
@@ -8,7 +8,7 @@
 
 void action_install_titledb(linked_list* items, list_item* selected) {
     char url[128];
-    snprintf(url, sizeof(url), "http://api.titledb.ga:7080/v0/proxy/%016llX", ((titledb_info*) selected->data)->titleId);
+    snprintf(url, sizeof(url), "https://api.titledb.ga:7443/v0/proxy/%016llX", ((titledb_info*) selected->data)->titleId);
 
     action_url_install("Install the selected title from TitleDB?", url, NULL, NULL);
 }
